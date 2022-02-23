3d16b92c088e9256f5ee9a9df6c55a33bf69b82e
JohnnyElvis <jayjay89@gmail.com>
2022-02-23 15:20:22 +0100

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+#Install unzip
+sudo apt-get update && sudo apt-get install unzip -y
+
 #Download GPU drivers
 wget -O /tmp/gpu.zip http://www.aspeedtech.com/file/support/Linux_DRM_1_12_3.zip && unzip /tmp/gpu.zip –d /tmp/gpu && chmod +x /tmp/gpu/Linux_DRM/DKMS/ast-drm-linux5.13.deb && .//tmp/gpu/Linux_DRM/DKMS/ast-drm-linux5.13.deb
