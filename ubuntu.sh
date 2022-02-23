a5ede4e782e982f437860bdc283c0dfb88a3a8c8
JohnnyElvis <jayjay89@gmail.com>
2022-02-23 15:16:26 +0100

    Create ubuntu.sh

---
+++ ubuntu.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#Download GPU drivers
+wget -O /tmp/gpu.zip http://www.aspeedtech.com/file/support/Linux_DRM_1_12_3.zip && unzip /tmp/gpu.zip –d /tmp/gpu && chmod +x /tmp/gpu/Linux_DRM/DKMS/ast-drm-linux5.13.deb && .//tmp/gpu/Linux_DRM/DKMS/ast-drm-linux5.13.deb
