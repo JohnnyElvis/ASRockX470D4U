0f8b76ed1b6bfa521a5946aa27643bcd0579b76d
JohnnyElvis <jayjay89@gmail.com>
2022-02-25 15:34:57 +0100

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -6,3 +6,4 @@ sudo apt-get update && sudo apt-get install unzip dkms linux-headers-$(uname -r)
 #Download GPU drivers
 cd /tmp && wget -O /tmp/gpu.zip http://www.aspeedtech.com/file/support/Linux_DRM_1_12_3.zip && unzip gpu.zip -d /tmp && sudo dpkg -i /tmp/Linux_DRM/DKMS/ast-drm-linux5.13.deb
 
+cd /tmp && wget http://repo.radeon.com/amdgpu-install/latest/ubuntu/focal/amdgpu-install_21.50.50000-1_all.deb sudo dpkg -i /tmp/amdgpu-install_21.50.50000-1_all.deb
