6383dc9e4fc1050ee5159f7c5450aae1071a53e4
JohnnyElvis <jayjay89@gmail.com>
2022-03-10 18:25:49 +0100

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Install unzip
-sudo apt-get update && sudo apt-get install unzip dkms linux-headers-$(uname -r) -y
+sudo apt-get update && sudo apt-get install dialog unzip dkms linux-headers-$(uname -r) -y
 
 #Download GPU drivers
 cd /tmp && wget -O /tmp/gpu.zip http://www.aspeedtech.com/file/support/Linux_DRM_1_12_3.zip && unzip gpu.zip -d /tmp && sudo dpkg -i /tmp/Linux_DRM/DKMS/ast-drm-linux5.13.deb
